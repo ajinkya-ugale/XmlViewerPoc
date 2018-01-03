5f3086cf9e92903c5d89e181ca2613ba2faabaad
ajinkya-ugale <ajinkya.ugale@springernature.com>
2018-01-03 15:15:44 +0530

    Remove: Removed Unwanted Files from git

--- src/Xml.Result.Processor/ResultXmlProcessor.cs
+++ src/Xml.Result.Processor/ResultXmlProcessor.cs
@@ -2,27 +2,27 @@
 
 namespace Xml.Result.Processor
 {
-    public class ResultXmlProcessor
-    {
-      private XDocument _diffXDoc;
-      private XDocument _resultXDoc;
+  public class ResultXmlProcessor
+  {
+    private XDocument _diffXDoc;
+    private XDocument _resultXDoc;
     public void StartResultProcessing(XDocument diffXDoc, XDocument resultXDoc)
     {
-      AddCrestId addCrestId=new AddCrestId();
-      _resultXDoc = addCrestId.AddCrestID(diffXDoc,resultXDoc);
-      ChangeFunctionality changeFunctionality=new ChangeFunctionality();
+      AddCrestId addCrestId = new AddCrestId();
+      _resultXDoc = addCrestId.AddCrestID(diffXDoc, resultXDoc);
+      ChangeFunctionality changeFunctionality = new ChangeFunctionality();
       _resultXDoc = changeFunctionality.AddNodeAndAttributeInformation(diffXDoc, resultXDoc);
-      DeleteFunctionality deleteFunctionality=new DeleteFunctionality();
+      DeleteFunctionality deleteFunctionality = new DeleteFunctionality();
       _resultXDoc = deleteFunctionality.AddMovedAndDeletedNodeAndAttributeInformation(diffXDoc, _resultXDoc);
-      AddFunctionality addFunctionality=new AddFunctionality();
+      AddFunctionality addFunctionality = new AddFunctionality();
       _resultXDoc = addFunctionality.AddNodeAttributeAndNode(diffXDoc, _resultXDoc);
-      ModificationFunctionalitiy modificationFunctionalitiy=new ModificationFunctionalitiy();
+      ModificationFunctionalitiy modificationFunctionalitiy = new ModificationFunctionalitiy();
       SaveResultXML(modificationFunctionalitiy.ModifyResult(_resultXDoc));
     }
 
     private void SaveResultXML(XDocument resultXDoc)
-      {
-        resultXDoc.Save(@"d:\SampleXML\result.xml");
-      }
+    {
+      resultXDoc.Save(@"d:\SampleXML\result.xml");
+    }
   }
 }
